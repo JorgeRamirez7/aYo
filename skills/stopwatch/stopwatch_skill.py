7bf34aa15b4bb5912bc20d8e299a6604469ff7fa
JorgeRamirez7 <jorgeramirezradilla@gmail.com>
2021-05-12 00:43:28 -0700

    Refactor stopwatch skill - replace global variables with global dictionary

--- skills/stopwatch/stopwatch_skill.py
+++ skills/stopwatch/stopwatch_skill.py
@@ -3,47 +3,41 @@ import logging
 
 from utils.readable_time_output import ReadableTimeOutput
 
-start_time = None
-end_time = None
+_times = {
+    "start_time": None,
+    "end_time": None
+    }
 
 class StopwatchSkill():
     def start_stopwatch(self):
-        global start_time
-
-        if start_time is None:
-            start_time = time.time()
+        if _times["start_time"] is None:
+            _times["start_time"] = time.time()
             return "Stopwatch has started."
         else:
             return "Stopwatch is already running."
 
     def stop_stopwatch(self):
-        global start_time, end_time
-
-        if start_time is None:
+        if _times["start_time"] is None:
             return "Try starting a stopwatch first."
         else:
-            end_time = time.time()
+            _times["end_time"] = time.time()
             return self.stopwatch_output()
 
     def reset_stopwatch(self):
-        global start_time, end_time
-
-        if start_time is None:
+        if _times["start_time"] is None:
             return "There is no stopwatch to reset"
 
-        start_time = None
-        end_time = None
+        _times["start_time"] = None
+        _times["end_time"] = None
         return "Stopwatch has been reset"
 
     def stopwatch_output(self):
-        global start_time, end_time
-
-        if start_time is None:
+        if _times["start_time"] is None:
             return "Error. Stopwatch has not started."
-        if end_time is None:
+        if _times["end_time"] is None:
             return "Error. Stopwatch has not ended."
 
-        seconds_total = end_time - start_time
+        seconds_total = _times["end_time"] - _times["start_time"]
 
         stopwatch_time = {
             "seconds": seconds_total,
