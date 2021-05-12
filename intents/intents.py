a88f1d3f711f0ef0151fe3540e7215afffefdbf7
JorgeRamirez7 <jorgeramirezradilla@gmail.com>
2021-05-12 16:37:25 -0700

    Refactor intents from main to separate intents class

---
+++ intents/intents.py
@@ -0,0 +1,18 @@
+from intents.web_search_intents import WebSearchIntents
+from intents.open_documentation_intents import OpenDocumentationIntents
+from intents.search_documentation_intents import SearchDocumentationIntents
+from intents.music_intents import MusicIntents
+
+class Intents():
+    def intents(self, user_input):
+        if user_input[-1] == '?' or user_input.split()[0] == "Search":
+            return WebSearchIntents().web_search_intents(user_input)
+    
+        elif user_input.split()[0] == "Open":
+            return OpenDocumentationIntents().open_documentation_intents(user_input)
+
+        elif user_input.split()[0] == "C++" or user_input.split()[0] == "Python":
+            return SearchDocumentationIntents().search_documentation_intents(user_input)
+
+        elif user_input == "Play music.":
+            return MusicIntents().music_intents(user_input)
--- main.py
+++ main.py
@@ -1,28 +1,14 @@
 from azure.microphone_input import MicrophoneInput
-from intents.web_search_intents import WebSearchIntents
-from intents.open_documentation_intents import OpenDocumentationIntents
-from intents.search_documentation_intents import SearchDocumentationIntents
-from intents.music_intents import MusicIntents
+from intents.intents import Intents
 
 ayo_result = False
 
 # Example to show voice command for opening webpage as well as searching google
 print("Begin speaking...")
 user_input = MicrophoneInput().get_voice_input()
+
 if user_input:
-
-    if user_input[-1] == '?' or user_input.split()[0] == "Search":
-        ayo_result = WebSearchIntents().web_search_intents(user_input)
-    
-    elif user_input.split()[0] == "Open":
-        ayo_result = OpenDocumentationIntents().open_documentation_intents(user_input)
-
-    elif user_input.split()[0] == "C++" or user_input.split()[0] == "Python":
-        ayo_result = SearchDocumentationIntents().search_documentation_intents(user_input)
-
-    elif user_input == "Play music.":
-        ayo_result = MusicIntents().music_intents(user_input)
-
+    ayo_result = Intents().intents(user_input)
 
 # Use hashtable of keywords
 # str.split() for first word in a string
