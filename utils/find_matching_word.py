512ac9160fda705e248cd104c52e6b413b148dea
Briley Hanson <hansonb7@spu.edu>
2021-05-14 16:18:07 -0700

    added spotify keywords

---
+++ utils/find_matching_word.py
@@ -0,0 +1,17 @@
+import re
+
+class FindMatchingWord(object):
+    words_for_start = ['start', 'begin', 'initiate', 'commence', 'set']
+    words_for_stop = ['end', 'stop ', 'terminate', 'finish', 'halt', 'conclude', 'close']
+    words_for_reset = ['reset', 'cancel']
+    words_for_stopwatch = ['stopwatch']
+    words_for_timer = ['timer', 'alarm']
+
+    words_for_spotify = ['play', 'next', 'previous', 'pause', 'resume', 'shuffle']
+
+    def find_match(self, input, words_list):
+        for word in words_list:
+            match_found = re.search(r"\b{}\b".format(word), input, re.IGNORECASE)
+            if match_found:
+                return True
+        return False
