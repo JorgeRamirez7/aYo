812cf0689c6613e6140f5686293cd7dca3058f37
JorgeRamirez7 <jorgeramirezradilla@gmail.com>
2021-05-16 15:27:33 -0700

    Refactor main to own function, following proper Python procedures

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from azure.microphone_input import MicrophoneInput
 from intents.intents import Intents
 
-print("Begin speaking...")
-user_input = MicrophoneInput().get_voice_input()
+def main():
+    print("Begin speaking...")
+    user_input = MicrophoneInput().get_voice_input()
 
-if user_input:
-    ayo_result = Intents().intents(user_input)
-    if ayo_result is not None and user_input != "":
-        print(ayo_result)
+    if user_input:
+        ayo_result = Intents().intents(user_input)
+        if ayo_result is not None and user_input != "":
+            print(ayo_result)
+
+if __name__ == "__main__":
+    main()
 
 # Use hashtable of keywords
 # str.split() for first word in a string
