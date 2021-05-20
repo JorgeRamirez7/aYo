6c698114e5908d9c906f8270d3b51d8ae80df224
JorgeRamirez7 <jorgeramirezradilla@gmail.com>
2021-05-20 15:13:10 -0700

    Refactor functionality of ImportDialogue to a single function

--- utils/import_dialogue.py
+++ utils/import_dialogue.py
@@ -4,11 +4,11 @@ import logging
 import yaml
 
 class ImportDialogue():
-    def import_dialogue(self, file_name:str):
-        """Imports dialogue from a given YAML file.
-        
+    def initialize_dialogue(self, dialogue_reference:str):
+        """Imports dialogue for a user query from a YAML file.'.
+            
             Args:
-                file_name: The name of the YAML file, including the extension '.YAML'.
+                dialogue_reference: The reference to a dialogue file name, configured under 'dialogue' in ayo.ini.
 
             Returns:
                 All values in the given file in the form of a dictionary.
@@ -16,6 +16,8 @@ class ImportDialogue():
         """
         config = configparser.ConfigParser()
         config.read('config/ayo.ini')
+
+        file_name = config.get('dialogue', dialogue_reference)
 
         ayo_localization = config.get('general', 'localization')
         folder_directory = config.get('dialogue', 'directory')
@@ -30,18 +32,3 @@ class ImportDialogue():
         except:
             logging.warning("Could not load dialogue located in {0}".format(file_directory))
             return None
-
-    def initialize_dialogue(self, dialogue_reference:str):
-        """Imports dialogue for a user query from a YAML file.'.
-            
-            Args:
-                dialogue_reference: The reference to a dialogue file name, configured under 'dialogue' in ayo.ini.
-
-            Returns:
-                ImportDialogue() object
-        """
-        config = configparser.ConfigParser()
-        config.read('config/ayo.ini')
-
-        dialogue_file_name = config.get('dialogue', dialogue_reference)
-        return ImportDialogue().import_dialogue(dialogue_file_name)
