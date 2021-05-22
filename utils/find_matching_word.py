5648359b198e69f8fa44bfae279c9799900cf3da
Briley Hanson <hansonb7@spu.edu>
2021-05-21 19:46:55 -0700

    added music player keywords

--- utils/find_matching_word.py
+++ utils/find_matching_word.py
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-import re
-
-class FindMatchingWord(object):
-    words_for_start = ['start', 'begin', 'initiate', 'commence', 'set']
-    words_for_stop = ['end', 'stop ', 'terminate', 'finish', 'halt', 'conclude', 'close']
-    words_for_reset = ['reset', 'cancel']
-    words_for_stopwatch = ['stopwatch']
-    words_for_timer = ['timer', 'alarm']
-
-    words_for_spotify = ['play', 'next', 'previous', 'pause', 'resume', 'shuffle']
-
-    def find_match(self, input, words_list):
-        for word in words_list:
-            match_found = re.search(r"\b{}\b".format(word), input, re.IGNORECASE)
-            if match_found:
-                return True
-        return False
-=======
 """Search for a given string in a given list."""
 import configparser
 import re
@@ -52,4 +33,3 @@ class FindMatchingWord():
                 return True
         
         return False
->>>>>>> main
