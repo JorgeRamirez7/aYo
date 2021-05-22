843efe493250cf33b3d2e17bc40aa822489a0d09
Briley Hanson <hansonb7@spu.edu>
2021-05-21 19:35:53 -0700

    positioned stopwatch conditional ahead of music

--- intents/intents.py
+++ intents/intents.py
@@ -2,12 +2,8 @@
 from intents.music_intents import MusicIntents
 from intents.open_documentation_intents import OpenDocumentationIntents
 from intents.search_documentation_intents import SearchDocumentationIntents
-<<<<<<< HEAD
-from intents.music_intents import MusicIntents
-=======
 from intents.stopwatch_intents import StopwatchIntents
 from intents.web_search_intents import WebSearchIntents
->>>>>>> main
 from utils.find_matching_word import FindMatchingWord
 
 class Intents():
@@ -21,11 +17,11 @@ class Intents():
         elif user_input.split()[0] == "C++" or user_input.split()[0] == "Python":
             return SearchDocumentationIntents().search_documentation_intents(user_input)
 
-        elif FindMatchingWord().find_match(str(user_input.split()[0]), FindMatchingWord.words_for_spotify):
-            return MusicIntents().music_intents(user_input)
-
         elif FindMatchingWord().find_match(user_input, FindMatchingWord().query["stopwatch"]):
             return StopwatchIntents().stopwatch_intent(user_input)
         
+        elif FindMatchingWord().find_match(user_input, FindMatchingWord().query["music"]):
+            return MusicIntents().music_intents(user_input)
+
         else:
             return "Hmm, I don't understand that"
