ab9dbe68f7a7da765cea3d28bdc1383f8e38877d
Briley Hanson <hansonb7@spu.edu>
2021-05-25 18:16:13 -0700

    all requested p/l's doc website added

--- UI/aYo_ui_main.py
+++ UI/aYo_ui_main.py
@@ -1,6 +1,5 @@
 import sys
 
-#unsure why this is still erroring, It works fine but I dont know if its something to be worried about
 from ui_classes.ayo_login import AyoLogin
 from PyQt5 import QtWidgets
 
