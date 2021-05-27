44f579d075030e581942b68f029a5ecf6e60e949
JorgeRamirez7 <jorgeramirezradilla@gmail.com>
2021-05-27 13:57:39 -0700

    Refactor intents.py to import user-queries from init instead of FindMatchingWord

--- intents/intents.py
+++ intents/intents.py
@@ -6,8 +6,12 @@ from intents.search_documentation_intents import SearchDocumentationIntents
 from intents.stopwatch_intents import StopwatchIntents
 from intents.web_search_intents import WebSearchIntents
 from utils.find_matching_word import FindMatchingWord
+from utils.import_dialogue import ImportDialogue
 
 class Intents():
+    def __init__(self):
+        self.user_queries = ImportDialogue().import_dialogue("user-queries.yaml")
+
     def intents(self, user_input):
         if user_input[-1] == '?' or user_input.split()[0] == "Search":
             return WebSearchIntents().web_search_intents(user_input)
@@ -18,13 +22,13 @@ class Intents():
         elif user_input.split()[0] == "C++" or user_input.split()[0] == "Python":
             return SearchDocumentationIntents().search_documentation_intents(user_input)
 
-        elif FindMatchingWord().find_match(user_input, FindMatchingWord().query["stopwatch"]):
+        elif FindMatchingWord().find_match(user_input, self.user_queries["stopwatch"]):
             return StopwatchIntents().stopwatch_intent(user_input)
 
-        elif FindMatchingWord().find_match(user_input, FindMatchingWord().query["alarm"]):
+        elif FindMatchingWord().find_match(user_input, self.user_queries["alarm"]):
             return AlarmTimerIntents().alarm_timer_intent(user_input)
         
-        elif FindMatchingWord().find_match(user_input, FindMatchingWord().query["music"]):
+        elif FindMatchingWord().find_match(user_input, self.user_queries["music"]):
             return MusicIntents().music_intents(user_input)
 
         else:
