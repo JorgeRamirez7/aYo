5e19393d4f2e0ae211c540083530fd9fead2da19
JorgeRamirez7 <jorgeramirezradilla@gmail.com>
2021-05-29 02:44:32 -0700

    Add user apologies to conversation_intents

--- intents/conversation_intents.py
+++ intents/conversation_intents.py
@@ -8,11 +8,13 @@ class ConversationIntents():
     jokes_queries = ImportDialogue().import_dialogue("conversation/jokes.yaml")
     greetings_queries = ImportDialogue().import_dialogue("conversation/greetings.yaml")
     ayo_functions_queries = ImportDialogue().import_dialogue("conversation/ayo-functions.yaml")
+    user_apologies_queries = ImportDialogue().import_dialogue("conversation/user-apologies.yaml")
     unknown_queries = ImportDialogue().import_dialogue("conversation/unknown.yaml")
 
     jokes = RandomElementSkill(jokes_queries["program-response"])
     greetings = RandomElementSkill(greetings_queries["program-response"])
     ayo_functions = RandomElementSkill(ayo_functions_queries["program-response"])
+    user_apologies = RandomElementSkill(user_apologies_queries["program-response"])
     unknown = RandomElementSkill(unknown_queries["program-response"])
 
     def conversation_intents(self, user_input:str) -> str:
@@ -32,6 +34,9 @@ class ConversationIntents():
 
         elif FindMatchingWord().find_match(user_input, self.ayo_functions_queries["user-trigger"]):
             return self.ayo_functions.get_random_element()
+
+        elif FindMatchingWord().find_match(user_input, self.user_apologies_queries["user-trigger"]):
+            return self.user_apologies.get_random_element()
 
         else:
             return self.unknown.get_random_element()
