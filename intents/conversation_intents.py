3bed0738d2406010debdf8260ddda411a9e65a34
JorgeRamirez7 <jorgeramirezradilla@gmail.com>
2021-05-29 02:56:06 -0700

    Add how-am-i queries to conversation intents

--- intents/conversation_intents.py
+++ intents/conversation_intents.py
@@ -8,12 +8,14 @@ class ConversationIntents():
     jokes_queries = ImportDialogue().import_dialogue("conversation/jokes.yaml")
     greetings_queries = ImportDialogue().import_dialogue("conversation/greetings.yaml")
     ayo_functions_queries = ImportDialogue().import_dialogue("conversation/ayo-functions.yaml")
+    how_am_i_queries = ImportDialogue().import_dialogue("conversation/how-am-i.yaml")
     user_apologies_queries = ImportDialogue().import_dialogue("conversation/user-apologies.yaml")
     unknown_queries = ImportDialogue().import_dialogue("conversation/unknown.yaml")
 
     jokes = RandomElementSkill(jokes_queries["program-response"])
     greetings = RandomElementSkill(greetings_queries["program-response"])
     ayo_functions = RandomElementSkill(ayo_functions_queries["program-response"])
+    how_am_i = RandomElementSkill(how_am_i_queries["program-response"])
     user_apologies = RandomElementSkill(user_apologies_queries["program-response"])
     unknown = RandomElementSkill(unknown_queries["program-response"])
 
@@ -34,6 +36,9 @@ class ConversationIntents():
 
         elif FindMatchingWord().find_match(user_input, self.ayo_functions_queries["user-trigger"]):
             return self.ayo_functions.get_random_element()
+        
+        elif FindMatchingWord().find_match(user_input, self.how_am_i_queries["user-trigger"]):
+            return self.how_am_i.get_random_element()
 
         elif FindMatchingWord().find_match(user_input, self.user_apologies_queries["user-trigger"]):
             return self.user_apologies.get_random_element()
