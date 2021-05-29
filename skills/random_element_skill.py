3d867fec72f1a65b4ebc3823ec7355ee2f8ab57b
JorgeRamirez7 <jorgeramirezradilla@gmail.com>
2021-05-29 00:08:48 -0700

    Implement random_element_skill to obtain a random element from a list

--- skills/random_element_skill.py
+++ skills/random_element_skill.py
@@ -1,28 +1,52 @@
-"""Gets a unique random element from a list."""
+"""Gets a unique random element from a list.
+There is never a repeated list item for at least 4 calls. 
+Must be a minimum of 4 variables"""
 import random
+
 from collections import deque
 
-from utils.import_dialogue import ImportDialogue
-
 class RandomElementSkill():
-    def __init__(self, list):
-        self.list = list
-        random_stack = self.add_random_elements_to_stack(self.list)
-        self.stack = random_stack
+    def __init__(self, original_list):
+        """Copies the parameter original_list to it's own self variable and a randomized list.
+            
+            Args:
+                original_list: The list that will be randomized.
+        """
+        self.original_list = original_list
+        self.last_item = None
+        self.penultimate_item = None
+    
+        self.shuffle_random_list()
 
-    def add_random_elements_to_stack(self, list):
-        elements_stack = deque()
-        
-        while list:
-            random_element = random.choice(list)
-            elements_stack.append(random_element)
-            list.remove(random_element)
+        self.last_item = self.randomized_list[0]
+        self.penultimate_item = self.randomized_list[1]
 
-        return elements_stack
+    def shuffle_random_list(self):
+        """Shuffles the items in random_list."""
+        self.randomized_list = deque()
+    
+        for item in self.original_list:
+            self.randomized_list.append(item)
+      
+        random.shuffle(self.randomized_list)
+
+        """If the next 2 elements to be popped are the same as the 2 previously popped elements, then reshuffle random list."""
+        while(self.randomized_list[len(self.randomized_list) - 1] == self.last_item or 
+        self.randomized_list[len(self.randomized_list) - 1] == self.penultimate_item or
+        self.randomized_list[len(self.randomized_list) - 2] == self.last_item or 
+        self.randomized_list[len(self.randomized_list) - 2] == self.penultimate_item):
+            self.shuffle_random_list()
+
+        self.last_item = self.randomized_list[0]
+        self.penultimate_item = self.randomized_list[1]
 
     def get_random_element(self):
-        if not self.stack:
-            self.add_random_elements_to_stack(self.list)
-            
-        element = self.stack.pop()
-        return element
+        """Gets the random element from a list."""
+        """List must contain at least 4 elements."""
+        if len(self.original_list) < 4:
+            return "OH NOOOOO"
+
+        if not self.randomized_list:
+            self.shuffle_random_list()
+      
+        return self.randomized_list.pop()
