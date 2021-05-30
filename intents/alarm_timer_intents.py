5705fead21be14a3570dae90d8ffc3bd5f297d74
JorgeRamirez7 <jorgeramirezradilla@gmail.com>
2021-05-30 00:26:28 -0700

    Refactor usage of user-queries.yaml into different yaml files within dialogue

--- intents/alarm_timer_intents.py
+++ intents/alarm_timer_intents.py
@@ -1,27 +1,30 @@
 from skills.alarm_skill import AlarmSkill
 from skills.timer_skill import TimerSkill
 from utils.find_matching_word import FindMatchingWord
+from utils.import_dialogue import ImportDialogue
 
 class AlarmTimerIntents():
     """Search for a Alarm/Timer-related query and perform the desired intent."""
+    alarm_timer_queries = ImportDialogue().import_dialogue("intents/alarm-timer.yaml")
+    verb_queries = ImportDialogue().import_dialogue("intents/verbs.yaml")
 
     def alarm_timer_intent(self, ayo_input:str) -> str:
         alarm_timer_output = None
 
-        if FindMatchingWord().find_match(ayo_input, FindMatchingWord.query["trigger-alarm"]):
+        if FindMatchingWord().find_match(ayo_input, self.alarm_timer_queries["trigger-alarm"]):
             alarm_timer_output = AlarmSkill().set_alarm(ayo_input)
         
-        elif FindMatchingWord().find_match(ayo_input, FindMatchingWord.query["trigger-timer"]):
+        elif FindMatchingWord().find_match(ayo_input, self.alarm_timer_queries["trigger-timer"]):
             alarm_timer_output = TimerSkill().set_timer(ayo_input)
             
-        elif (  FindMatchingWord().find_match(ayo_input, FindMatchingWord.query["stop"]) or 
-                FindMatchingWord().find_match(ayo_input, FindMatchingWord.query["reset"])
+        elif (  FindMatchingWord().find_match(ayo_input, self.verb_queries["stop"]) or 
+                FindMatchingWord().find_match(ayo_input, self.verb_queries["reset"])
                 ):
 
-            if FindMatchingWord().find_match(ayo_input, FindMatchingWord.query["timer-only"]):
+            if FindMatchingWord().find_match(ayo_input, self.alarm_timer_queries["timer-only"]):
                 alarm_timer_output = TimerSkill().cancel_timer()
 
-            elif FindMatchingWord().find_match(ayo_input, FindMatchingWord.query["alarm-only"]): 
+            elif FindMatchingWord().find_match(ayo_input, self.alarm_timer_queries["alarm-only"]): 
                 alarm_timer_output = AlarmSkill().cancel_alarm()
 
             else:
@@ -29,10 +32,10 @@ class AlarmTimerIntents():
 
         else:
 
-            if FindMatchingWord().find_match(ayo_input, FindMatchingWord.query["timer-only"]):
+            if FindMatchingWord().find_match(ayo_input, self.alarm_timer_queries["timer-only"]):
                 alarm_timer_output = TimerSkill().generic_response()
 
-            elif FindMatchingWord().find_match(ayo_input, FindMatchingWord.query["alarm-only"]):
+            elif FindMatchingWord().find_match(ayo_input, self.alarm_timer_queries["alarm-only"]):
                 alarm_timer_output = AlarmSkill().generic_response()
 
             else:
