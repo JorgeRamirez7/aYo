657088fc0754efc7221973d5501f571d4e165395
JorgeRamirez7 <jorgeramirezradilla@gmail.com>
2021-05-30 00:05:38 -0700

    Refactor import_dialogue to do config actions outside of import_dialogue

--- utils/import_dialogue.py
+++ utils/import_dialogue.py
@@ -4,6 +4,17 @@ import yaml
 
 class ImportDialogue():
     """Import a YAML file into a dictionary."""
+
+    try:
+        config = configparser.ConfigParser()
+        config.read('config/ayo.ini')
+
+        ayo_localization = config.get('general', 'localization')
+        folder_directory = config.get('dialogue', 'directory')
+
+    except:
+        logging.critical("Could not load 'general' or 'dialogue' from ayo.ini - aYo cannot function without these.")
+
     def import_dialogue(self, file_name:str):
         """Imports dialogue from a given YAML file.
         
@@ -13,13 +24,7 @@ class ImportDialogue():
                 All values in the given file in the form of a dictionary.
                 None if the YAML file was not able to be loaded.
         """
-        config = configparser.ConfigParser()
-        config.read('config/ayo.ini')
-
-        ayo_localization = config.get('general', 'localization')
-        folder_directory = config.get('dialogue', 'directory')
-
-        file_directory = "{0}{1}\{2}".format(folder_directory, ayo_localization, file_name)
+        file_directory = "{0}{1}\{2}".format(self.folder_directory, self.ayo_localization, file_name)
 
         try:
             with open(file_directory) as file:
