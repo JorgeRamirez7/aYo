c3086e36937d30915c8ca84c4dd3ffb8dcaac276
JorgeRamirez7 <jorgeramirezradilla@gmail.com>
2021-05-29 17:32:12 -0700

    Add and implement introduction for user in main and introduction skill

--- __main__.py
+++ __main__.py
@@ -1,17 +1,21 @@
 from azure.microphone_input import MicrophoneInput
 from azure.text_to_speech import TextToSpeech
 from intents.intents import Intents
+from skills.introduction_skill import IntroductionSkill
+
+ayo_is_running = True
+ayo_introduction = True
+
+microphone_input = True
+text_to_speech = True
 
 def main():
-    MICROPHONE_INPUT = True
-    TEXT_TO_SPEECH = True
-
-    ayo_is_running = True
+    global ayo_is_running, introduction, microphone_input, text_to_speech
 
     while ayo_is_running:
         print("Begin speaking...")
 
-        if MICROPHONE_INPUT:
+        if microphone_input:
             user_input = MicrophoneInput().get_voice_input()
         else:
             user_input = input()
@@ -19,7 +23,7 @@ def main():
         if user_input:
             ayo_result = Intents().intents(user_input)
             if ayo_result is not None and user_input != "":
-                if TEXT_TO_SPEECH:
+                if text_to_speech:
                     TextToSpeech().text_to_speech(ayo_result)
                 else:
                     print(ayo_result)
@@ -27,4 +31,7 @@ def main():
         print()
 
 if __name__ == "__main__":
+    if ayo_introduction:
+        IntroductionSkill().ayo_intro()
+
     main()
---
+++ skills/introduction_skill.py
@@ -0,0 +1,15 @@
+from azure.text_to_speech import TextToSpeech
+from skills.random_element_skill import RandomElementSkill
+from utils.find_matching_word import FindMatchingWord
+from utils.import_dialogue import ImportDialogue
+
+class IntroductionSkill():
+    """Introduction/greeting when a user start aYo."""
+    introduction_queries = ImportDialogue().import_dialogue("introductions.yaml")
+    introduction = RandomElementSkill(introduction_queries["program-response"])
+
+    def ayo_intro(self):
+        """Says a random introduction."""
+        random_intro = self.introduction.get_random_element()
+        TextToSpeech().text_to_speech(random_intro)
+        
