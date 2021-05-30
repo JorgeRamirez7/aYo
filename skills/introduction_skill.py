5a607328d0066bbfdaa5af5d07e9980cf95e7bbe
JorgeRamirez7 <jorgeramirezradilla@gmail.com>
2021-05-30 01:09:42 -0700

    Add intro sfx for introduction_skill

--- skills/introduction_skill.py
+++ skills/introduction_skill.py
@@ -1,3 +1,5 @@
+from playsound import playsound
+
 from azure.text_to_speech import TextToSpeech
 from skills.random_element_skill import RandomElementSkill
 from utils.find_matching_word import FindMatchingWord
@@ -5,11 +7,13 @@ from utils.import_dialogue import ImportDialogue
 
 class IntroductionSkill():
     """Introduction/greeting when a user start aYo."""
+    _intro_sfx = 'data/intro.wav'
     introduction_queries = ImportDialogue().import_dialogue("skills/introductions.yaml")
     introduction = RandomElementSkill(introduction_queries["program-response"])
 
     def ayo_intro(self):
-        """Says a random introduction."""
+        """Plays sfx and says a random introduction."""
+        playsound(self._intro_sfx)
         random_intro = self.introduction.get_random_element()
         TextToSpeech().text_to_speech(random_intro)
         
