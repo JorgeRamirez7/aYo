06870d743c8555d0867f117b8ba6dce6b5b53c2d
JorgeRamirez7 <jorgeramirezradilla@gmail.com>
2021-05-31 11:40:50 -0700

    Implement 'hey-yo' keyword for voice activation

--- azure/get_query.py
+++ azure/get_query.py
@@ -1,3 +1,4 @@
+from azure.ayo_keyword import AyoKeyword
 from azure.microphone_input import MicrophoneInput
 from azure.text_to_speech import TextToSpeech
 from intents.intents import Intents
@@ -5,17 +6,25 @@ from intents.intents import Intents
 class GetQuery():
     def get_query(self):
         """Gets the query from a user input via text or speech and outputs it via text and/or speech."""
+        ayo_keyword = True
         microphone_input = True
         text_to_speech = True
 
         ayo_is_running = True
 
         while ayo_is_running:
-            print("Begin speaking...")
 
-            if microphone_input:
+            if ayo_keyword:
+                AyoKeyword().ayo_keyword()
+                print("Start speaking")
                 user_input = MicrophoneInput().get_voice_input()
+            
+            elif microphone_input:
+                print("Begin speaking...")
+                user_input = MicrophoneInput().get_voice_input()
+            
             else:
+                print("Begin typing...")
                 user_input = input()
 
             if user_input:
