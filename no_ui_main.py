1761bb3b11bdaa6e9da41d18f0b22c59cd9bee98
JorgeRamirez7 <jorgeramirezradilla@gmail.com>
2021-05-31 15:29:55 -0700

    Add main that does not include main for quicker testing/debug

---
+++ no_ui_main.py
@@ -0,0 +1,10 @@
+from azure.get_query import GetQuery
+from skills.introduction_skill import IntroductionSkill
+
+if __name__ == "__main__":
+    ayo_introduction = False
+
+    if ayo_introduction:
+        IntroductionSkill().ayo_intro()
+    
+    GetQuery().query()
