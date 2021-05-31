8ffb586a822859485dac2b6c7d0201b767465043
JorgeRamirez7 <jorgeramirezradilla@gmail.com>
2021-05-31 15:30:11 -0700

    Add self to get_query

--- azure/get_query.py
+++ azure/get_query.py
@@ -3,7 +3,7 @@ from azure.text_to_speech import TextToSpeech
 from intents.intents import Intents
 
 class GetQuery():
-    def query():
+    def query(self):
         """Gets the query from a user input via text or speech and outputs it via text and/or speech."""
         microphone_input = True
         text_to_speech = True
