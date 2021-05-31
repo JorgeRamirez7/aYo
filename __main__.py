22c669957c8b10f004bca6e1222cd3e83610f47f
JorgeRamirez7 <jorgeramirezradilla@gmail.com>
2021-05-31 14:15:47 -0700

    Readd introduction skill to main

--- __main__.py
+++ __main__.py
@@ -1,8 +1,10 @@
 from PyQt5 import QtWidgets
+from skills.introduction_skill import IntroductionSkill
 
 from ui.ui_classes.ayo_login import AyoLogin
 
 if __name__ == "__main__":
+    IntroductionSkill().ayo_intro()
     #If this is giving you an error add "python.analysis.extraPaths": ["./UI"] to your settings.json file
     app = QtWidgets.QApplication([])
     win = AyoLogin()
