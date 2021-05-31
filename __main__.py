c76c7446cf6d375a2745fc9738372a2404638dc0
JorgeRamirez7 <jorgeramirezradilla@gmail.com>
2021-05-31 15:14:48 -0700

    Comment out intro skill in main to show that it is possible

--- __main__.py
+++ __main__.py
@@ -4,7 +4,10 @@ from skills.introduction_skill import IntroductionSkill
 from ui.ui_classes.ayo_login import AyoLogin
 
 if __name__ == "__main__":
-    IntroductionSkill().ayo_intro()
+
+    """Plays intro sfx and greets user."""
+    #IntroductionSkill().ayo_intro()
+
     #If this is giving you an error add "python.analysis.extraPaths": ["./UI"] to your settings.json file
     app = QtWidgets.QApplication([])
     win = AyoLogin()
