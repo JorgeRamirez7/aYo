a1621189be1389c592770abe01c76d092b49afbb
Briley Hanson <hansonb7@spu.edu>
2021-05-30 17:47:59 -0700

    error handling for play

--- intents/music_intents.py
+++ intents/music_intents.py
@@ -8,12 +8,32 @@ class MusicIntents(object):
         key = user_input.split()[0]
         
         if FindMatchingWord().find_match(user_input, FindMatchingWord.query["play"]):
+            try:
+                argument = user_input.split()[1]
+                print(argument)
+            except IndexError:
+                print("Say play artist <artist>, play podcast <podcast>, or play <song>")
+                return
+
+            
             if str(user_input.split()[1]).lower() == 'artist':
-                MusicSkill().play_artist(user_input)
+                try:
+                    word = user_input.split()[2]
+                    MusicSkill().play_artist(user_input)
+                except IndexError:
+                    print("Provide artist")
+                
             elif str(user_input.split()[1]).lower() == 'podcast':
-                MusicSkill().play_podcast(user_input)
-            else:
+                try:
+                    word = user_input.split()[2]
+                    MusicSkill().play_podcast(user_input)
+                except IndexError:
+                    print("Provide podcast")
+
+            else:    
                 MusicSkill().play_song(user_input)
+                
+
         elif FindMatchingWord().find_match(user_input, FindMatchingWord.query["next"]):
             MusicSkill().next()
         elif FindMatchingWord().find_match(user_input, FindMatchingWord.query["previous"]):
