872662a495bee1ad6b6fe7ca7e894ec62767a5e5
JorgeRamirez7 <jorgeramirezradilla@gmail.com>
2021-05-31 14:12:35 -0700

    Refactor main, eliminate unecessary calls

--- __main__.py
+++ __main__.py
@@ -1,20 +1,10 @@
 from PyQt5 import QtWidgets
 
-from azure.get_query import GetQuery
-from skills.introduction_skill import IntroductionSkill
 from ui.ui_classes.ayo_login import AyoLogin
 
 if __name__ == "__main__":
-    ayo_introduction = True
-
-    if ayo_introduction:
-        IntroductionSkill().ayo_intro()
-
-
-#If this is giving you an error add "python.analysis.extraPaths": ["./UI"] to your settings.json file
+    #If this is giving you an error add "python.analysis.extraPaths": ["./UI"] to your settings.json file
     app = QtWidgets.QApplication([])
     win = AyoLogin()
     win.show()
     app.exec()
-
-    GetQuery().query()
